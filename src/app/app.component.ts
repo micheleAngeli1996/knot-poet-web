6b8c57aa8e3f77f9a6fa2266efd328549abf7463
Michele Angeli <michele.angeli@gullivernet.com>
2025-06-21 19:47:37 +0200

    contact page (wip)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
-import {NgOptimizedImage} from '@angular/common';
 import {Toast} from 'primeng/toast';
 import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgOptimizedImage, Toast],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, Toast],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [MessageService]
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {CardModule} from 'primeng/card';
+import {TranslatePipe} from '@ngx-translate/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {TextareaModule} from 'primeng/textarea';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Button} from 'primeng/button';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {FloatLabel} from 'primeng/floatlabel';
+import {MessageService} from 'primeng/api';
+import {Toast} from 'primeng/toast';
 
 @Component({
   selector: 'contact',
-  imports: [],
+  imports: [CardModule, InputTextModule, TextareaModule, TranslatePipe, ReactiveFormsModule, FormsModule, Button, FloatLabel, Toast],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  private http = inject(HttpClient);
+  private messageService = inject(MessageService);
 
+  emailFromGroup = new FormGroup({
+    'name': new FormControl(),
+    'emailToReplay': new FormControl(),
+    'message': new FormControl()
+  });
+
+  onSubmit() {
+    if (this.emailFromGroup.valid) {
+      const emailToReplay = this.emailFromGroup!.get('emailToReplay')!.value;
+      const name = this.emailFromGroup!.get('name')!.value;
+      const message = this.emailFromGroup!.get('message')!.value;
+      const headers = new HttpHeaders({'Content-Type': 'application/json'});
+      this.http.post('https://formspree.io/f/xeokldav',
+        {name, replyto: emailToReplay, message}, {'headers': headers}).subscribe(
+        {
+          next: response => {
+            console.log(response);
+            /*this.messageService.add({
+              summary: 'Success',
+              severity: 'success',
+              detail: 'Message sent',
+              life: 3000
+            });*/
+            this.emailFromGroup.reset();
+          },
+          error: error => {
+            this.messageService.add({
+              summary: 'Error',
+              severity: 'error',
+              detail: 'An error occurred while sending the message',
+              life: 3000
+            })
+          },
+          complete: () => {
+            this.messageService.add({
+              summary: 'Success',
+              severity: 'success',
+              detail: 'Your message has been sent',
+            })
+          }
+        }
+      );
+    } else {
+      this.messageService.add({
+        summary: 'Error',
+        severity: 'error',
+        detail: 'Please fill all the fields',
+        life: 3000
+      })
+    }
+  }
 }
