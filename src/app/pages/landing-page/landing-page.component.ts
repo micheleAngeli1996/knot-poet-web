f8c5b48a7934a04df60d2b8c4e56a0671543c3c1
Michele Angeli <michele.angeli@gullivernet.com>
2025-06-25 10:20:36 +0200

    animation landing page (wip) + about animation (done)

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,20 +1,12 @@
 import {Component} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {AnimateOnScrollModule} from 'primeng/animateonscroll';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'landing-page',
   imports: [ButtonModule, AnimateOnScrollModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
-  animations: [
-    trigger('flyInOut', [
-      state('in', style({transform: 'translateX(0)'})),
-      transition('void => *', [style({transform: 'translateX(-100%)'}), animate(100)]),
-      transition('* => void', [animate(100, style({transform: 'translateX(100%)'}))]),
-    ]),
-  ],
 })
 export class LandingPageComponent {
 
