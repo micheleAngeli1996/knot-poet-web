0ae8f5774abe9c8b3cf3451bec7e86d21be2597c
Michele Angeli <michele.angeli@gullivernet.com>
2025-07-02 18:31:06 +0200

    try seo and icon

---
+++ src/app/services/email.service.ts
@@ -0,0 +1,15 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {EmailRequest} from '../models/EmailRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  private emailUrl = 'http://localhost:3000/send-email'; // Replace with your backend URL
+  private http = inject(HttpClient);
+
+  sendEmail(emailRequest: EmailRequest) {
+    return this.http.post(this.emailUrl, emailRequest);
+  }
+}
